512f766996ab9612c6958df8b11fb6d17e50da2a
Hisou10 <izakuro11@gmail.com>
2023-06-16 18:32:12 +0700

    change assets format

--- assets/game.js
+++ assets/game.js
@@ -79,7 +79,7 @@ const generateGame = () => {
   const numPairs = totalCards / 2;
   const numImages = Math.min(numPairs, 23);
   const baseURL = location.origin;
-  const imagePaths = Array.from({ length: numImages }, (_, index) => `./assets/Artwork/${index + 1}.png`);
+  const imagePaths = Array.from({ length: numImages }, (_, index) => `./assets/Artwork/${index + 1}.jpg`);
   const items = shuffle([...imagePaths, ...imagePaths]);
   const cards = `
       <div class="board" style="grid-template-columns: repeat(${dimensions}, auto)">
